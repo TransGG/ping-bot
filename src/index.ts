ab89c26ac341af5261cf204cbc9081db843f7781
Skyler Grey <skyler3665@gmail.com>
2022-06-06 10:06:36 +0100

    Comment unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Client, ClientOptions } from "discord.js";
-import { CommandManager } from "./discord/commandManager.js";
-import { MenuManager } from "./discord/menuManager.js";
-import { Framework, frameworkConfig } from "./framework.js";
+// import { CommandManager } from "./discord/commandManager.js";
+// import { MenuManager } from "./discord/menuManager.js";
+// import { Framework, frameworkConfig } from "./framework.js";
 
 const client = new Client({
   allowedMentions: { parse: [], repliedUser: true },
   presence: { activities: [{ name: "to your pings", type: "LISTENING" }] },
   intents: ["GUILD_MESSAGES"],
-});
+} as ClientOptions);
 
 client.login(process.env["token"]);
