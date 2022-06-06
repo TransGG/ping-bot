c98b19b3d3087714a3f27000156d9bb27db7aa5f
Skyler Grey <skyler3665@gmail.com>
2022-06-06 10:04:56 +0100

    ActivityType is not an enum

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Client, ClientOptions, ActivityType } from "discord.js";
+import { Client, ClientOptions } from "discord.js";
 import { CommandManager } from "./discord/commandManager.js";
 import { MenuManager } from "./discord/menuManager.js";
 import { Framework, frameworkConfig } from "./framework.js";
 
 const client = new Client({
   allowedMentions: { parse: [], repliedUser: true },
-  presence: { activities: [{ name: "to your pings", type: ActivityType.LISTENING }] },
+  presence: { activities: [{ name: "to your pings", type: "LISTENING" }] },
   intents: ["GUILD_MESSAGES"],
 });
 
