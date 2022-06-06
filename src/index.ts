818c26b1aca06998d228c80ce78c8d8ca1f3fc5e
Skyler Grey <skyler3665@gmail.com>
2022-06-06 09:53:34 +0100

    Add a basic bot in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Client, ClientOptions } from "discord.js";
+import { Client, ClientOptions, ActivityType } from "discord.js";
 import { CommandManager } from "./discord/commandManager.js";
 import { MenuManager } from "./discord/menuManager.js";
 import { Framework, frameworkConfig } from "./framework.js";
 
-export default Framework;
-export { Framework, Client, ClientOptions, CommandManager, MenuManager, frameworkConfig };
+const client = new Client({
+  allowedMentions: { parse: [], repliedUser: true },
+  presence: { activities: [{ name: "to your pings", type: ActivityType.LISTENING }] },
+  intents: ["GUILD_MESSAGES"],
+});
+
+client.login(process.env.TOKEN);
